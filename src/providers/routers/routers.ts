1c69b2515f06fb1ba0af1dfd8e07b697e8316eec
jesus Qunto <jesuskinto@gmail.com>
2018-06-16 02:11:09 -0400

    No funcional

--- src/providers/routers/routers.ts
+++ src/providers/routers/routers.ts
@@ -21,24 +21,34 @@ export class RoutersProvider {
   	this.position = 0;
     this.signalOut = false;
 
-    this.abc=[
-      'A','B','C','D',
-      'E','F','G','H',
-      'I','J','K','L',
-      'M','N','O','P',
-      'Q','R','S','T',
-      'U','V','W','X',
-      'Y','Z'
+    this.abc = [
+      ' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+      '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?',
+      '@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+      'P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_',
+      '\`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+      'p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','DEL',
+      '€',' ','¡','¢','£','¤','¥','¦','§','¨','©','ª','«','¬',' ','®','¯','°',
+      '±','²','³','´','µ','¶','·','¸','¹','º','»','¼','½','¾','¿','À','Á',
+      'Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò',
+      'Ó','Ô','Õ','Ö','×','Ø','Ù','Ú','Û','Ü','Ý','Þ','ß','à','á','â','ã',
+      'ä','å','æ','ç','è','é','ê','ë','ì','í','î','ï','ð','ñ','ò','ó','ô',
+      'õ','ö','÷','ø','ù','ú','û','ü','ý','þ','ÿ'
     ];
 
-    this.abcStatic=[
-      'A','B','C','D',
-      'E','F','G','H',
-      'I','J','K','L',
-      'M','N','O','P',
-      'Q','R','S','T',
-      'U','V','W','X',
-      'Y','Z'
+    this.abcStatic = [
+      ' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+      '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?',
+      '@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+      'P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_',
+      '\`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+      'p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','DEL',
+      '€',' ','¡','¢','£','¤','¥','¦','§','¨','©','ª','«','¬',' ','®','¯','°',
+      '±','²','³','´','µ','¶','·','¸','¹','º','»','¼','½','¾','¿','À','Á',
+      'Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò',
+      'Ó','Ô','Õ','Ö','×','Ø','Ù','Ú','Û','Ü','Ý','Þ','ß','à','á','â','ã',
+      'ä','å','æ','ç','è','é','ê','ë','ì','í','î','ï','ð','ñ','ò','ó','ô',
+      'õ','ö','÷','ø','ù','ú','û','ü','ý','þ','ÿ'
     ];
   }
 
@@ -62,15 +72,17 @@ export class RoutersProvider {
       //Aumentamos la posicion a posicion + 1
       this.position+=1;
 
-      //Verificamos si hemos girado mas de 25 veces, ponemos la posicion en 0 y enviamos una senal
-      if (this.position > 25) {
+      //Verificamos si hemos girado mas de 224 veces, ponemos la posicion en 0 y enviamos una senal
+      if (this.position > 224) {
         this.signalOut=true;
         this.position=0;
       }
     }
-
-    var inputNumber = (input.charCodeAt() - "A".charCodeAt(0));
-
+    if(input.charCodeAt() > '€'.charCodeAt(0))
+      var inputNumber = (input.charCodeAt() - " ".charCodeAt(0) -32);
+    else
+      var inputNumber = (input.charCodeAt() - " ".charCodeAt(0));
+    
     /*  1) this.abc[inputNumber]  Representa la Variable de entrada dentro del router
         2) this.abc.indexOf(1) Devuelve el numero de la posicion de la (1) en abc
         3) this.regInput[this.regInput.indexOf(2)] Retorna la letra que esta en la posicion de (2) dentro de regInput
@@ -94,7 +106,12 @@ export class RoutersProvider {
 
   encryptOutside(input)
   { 
-    var inputNumber = (input.charCodeAt() - "A".charCodeAt(0));
+
+    if(input.charCodeAt() > '€'.charCodeAt(0))
+      var inputNumber = (input.charCodeAt() - " ".charCodeAt(0) -32);
+    else
+      var inputNumber = (input.charCodeAt() - " ".charCodeAt(0));
+
    
     //Proceso de encriptacion:
     var encrypt = this.abc[this.regInput.indexOf(this.abc[inputNumber])];
@@ -108,15 +125,20 @@ export class RoutersProvider {
   {
     this.regInput= Object.assign([], this.regSelect);  
     this.position= 0;
-
-    this.abc=[
-      'A','B','C','D',
-      'E','F','G','H',
-      'I','J','K','L',
-      'M','N','O','P',
-      'Q','R','S','T',
-      'U','V','W','X',
-      'Y','Z'
+    
+    this.abc = [
+      ' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+      '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?',
+      '@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+      'P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_',
+      '\`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+      'p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','DEL',
+      '€',' ','¡','¢','£','¤','¥','¦','§','¨','©','ª','«','¬',' ','®','¯','°',
+      '±','²','³','´','µ','¶','·','¸','¹','º','»','¼','½','¾','¿','À','Á',
+      'Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò',
+      'Ó','Ô','Õ','Ö','×','Ø','Ù','Ú','Û','Ü','Ý','Þ','ß','à','á','â','ã',
+      'ä','å','æ','ç','è','é','ê','ë','ì','í','î','ï','ð','ñ','ò','ó','ô',
+      'õ','ö','÷','ø','ù','ú','û','ü','ý','þ','ÿ'
     ];
 
     return this.abc[0];
@@ -124,17 +146,27 @@ export class RoutersProvider {
 
   move(newposition)
   {
-    this.position = newposition.charCodeAt() - "A".charCodeAt(0);
+    if(newposition.charCodeAt() > '€'.charCodeAt(0))
+      this.position = (newposition.charCodeAt() - " ".charCodeAt(0) -32);
+    else
+      this.position = (newposition.charCodeAt() - " ".charCodeAt(0));
+    
     this.regInput= Object.assign([], this.regSelect); 
 
-    this.abc=[
-      'A','B','C','D',
-      'E','F','G','H',
-      'I','J','K','L',
-      'M','N','O','P',
-      'Q','R','S','T',
-      'U','V','W','X',
-      'Y','Z'
+    
+    this.abc = [
+      ' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+      '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?',
+      '@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+      'P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_',
+      '\`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+      'p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','DEL',
+      '€',' ','¡','¢','£','¤','¥','¦','§','¨','©','ª','«','¬',' ','®','¯','°',
+      '±','²','³','´','µ','¶','·','¸','¹','º','»','¼','½','¾','¿','À','Á',
+      'Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò',
+      'Ó','Ô','Õ','Ö','×','Ø','Ù','Ú','Û','Ü','Ý','Þ','ß','à','á','â','ã',
+      'ä','å','æ','ç','è','é','ê','ë','ì','í','î','ï','ð','ñ','ò','ó','ô',
+      'õ','ö','÷','ø','ù','ú','û','ü','ý','þ','ÿ'
     ];
   
     //Colocamos el router en la posicion definida por el usuario
